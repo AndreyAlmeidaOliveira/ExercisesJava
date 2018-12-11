f00f1a4fe6c538c7fcaf1085533dcef6db5c1c0d
Andrey_Oliveira <almeidao94@gmail.com>
2018-12-11 17:53:54 -0200

    Segunda versão

---
+++ src/br/com/caelum/contas/testeErros/TesteConta.java
@@ -0,0 +1,26 @@
+package br.com.caelum.contas.testeErros;
+
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
+import br.com.caelum.contas.modelo.ContaPoupanca;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		Conta cc = new ContaCorrente();
+		Conta cp = new ContaPoupanca();
+		cc.deposita(100);
+		cp.deposita(100);
+		//		System.out.println("Valor Conta Corrente $"+cc.getSaldo());
+
+		try {
+			cc.saca(102);
+			System.out.println("Saque efetuado com sucesso");
+		} catch (SaldoInsuficienteException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Valor Conta Corrente $"+cc.getSaldo());
+				
+	}
+
+}
